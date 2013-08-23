2748175789d86b67f10cdcd8c6f58be20f122632
Sergey Popov <poserg@gmail.com>
2013-08-23 19:01:53 +0400

    Add files for test. Update mask.

--- script.py
+++ script.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from os import *
 from fnmatch import fnmatch
 from shutil import copy2 as copy
 
-mask = '*aster*'
+# Файл на который нужно все заменить
+orig_file = 'MasterServer.vdf'
+
+# Заменяемые файлы
+mask2 = ['rev_MasterServers', 'MasterServers2', 'MasterServers']
+
+# Начальная директория для поиска
 start_dir = '.'
-orig_file = 'script.py'
 
 def walk(dir, mask):
     result = []
     for file in listdir(dir):
         file_path = path.join(dir, file)
         if path.isfile(file_path):
-            if fnmatch(file_path, mask):
-                result.append(file_path)
+            for m in mask:
+                if m == file and m != orig_file:
+                    result.append(file_path)
+                    break
         else:
-            walk(path)
+            result += walk(file_path, mask)
     return result
 
-files = walk(start_dir, mask)
-for i in files:
-    copy(orig_file, i)
+if path.exists(orig_file):
+    files = walk(start_dir, mask2)
+    for i in files:
+        copy(orig_file, i)
+        print 'Updated ' + i
