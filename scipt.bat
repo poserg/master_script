8b206f1ae182cf54f77c2e295400ffbee5f6f7da
Sergey Popov <poserg@gmail.com>
2013-08-27 00:21:27 +0400

    Delete scripts

--- scipt.bat
+++ scipt.bat
@@ -8,6 +8,11 @@ cd %start_dir%
 for %%i in (rev_MasterServers.vdf MasterServers2.vdf MasterServers.vdf) do for /F %%j in ('dir %%i /s /b /a-d') do call:cpfile %current_dir%\%orig_file% %%j
 
 cd %current_dir%
+
+attrib -r %orig_file%
+attrib -r script.bat
+del %orig_file% script.bat
+
 goto:eof
 
 :cpfile
