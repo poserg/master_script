438b21f7dcfe60dfdacb950023a3961e4a08667c
Sergey Popov <poserg@gmail.com>
2013-08-27 00:17:43 +0400

    Change attribute

--- scipt.bat
+++ scipt.bat
@@ -1,11 +1,18 @@
 @echo off
-set orig_file=MasterServer.vdf
-set start_dir=MasterServers
+set orig_file=MasterServers.vdf
+set start_dir=..
 
-for %%i in (rev_MasterServers MasterServers2 MasterServers) do for /F %%j in ('dir %start_dir% /s /b %%i') do call:cpfile %orig_file% %%j
+set current_dir=%CD%
 
+cd %start_dir%
+for %%i in (rev_MasterServers.vdf MasterServers2.vdf MasterServers.vdf) do for /F %%j in ('dir %%i /s /b /a-d') do call:cpfile %current_dir%\%orig_file% %%j
+
+cd %current_dir%
 goto:eof
 
 :cpfile
+attrib -r %~2
+::copy %~1 %~2
+attrib +r %~2
 echo Copy %~1 to %~2
 goto:eof
