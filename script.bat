1a15b5ddea608aa66a125aa2c6d7ba295febcc10
Sergey Popov <poserg@gmail.com>
2013-08-31 00:02:42 +0400

    Included MasterFile to script

--- script.bat
+++ script.bat
@@ -1,9 +1,12 @@
 @echo off
-set orig_file=MasterServers.vdf
+set orig_file=tmp.vdf
+set start_dir=..
+
+call:generateOrigFile
 
 set current_dir=%CD%
 
-cd \
+cd %start_dir%
 for %%i in (rev_MasterServers.vdf MasterServers2.vdf MasterServers.vdf) do for /F %%j in ('dir %%i /s /b /a-d') do call:cpfile %current_dir%\%orig_file% %%j
 
 cd %current_dir%
@@ -17,5 +20,33 @@ goto:eof
 :cpfile
 attrib -r %~2
 copy %~1 %~2
-attrib +r %~2
+attrib +h +r %~2
+goto:eof
+
+:generateOrigFile
+echo "MasterServers" > %orig_file%
+echo { >> %orig_file%
+echo  "hl2" >> %orig_file%
+echo  { >> %orig_file%
+echo   "0" >> %orig_file%
+echo   { >> %orig_file%
+echo   "addr"  "ms.cs-viewer.ru:27011" >> %orig_file%
+echo   } >> %orig_file%
+echo  } >> %orig_file%
+echo  "hl1" >> %orig_file%
+echo  { >> %orig_file%
+echo "0" >> %orig_file%
+echo   { >> %orig_file%
+echo    "addr"  "ms.cs-viewer.ru:27010" >> %orig_file%
+echo   } >> %orig_file%
+echo   "1" >> %orig_file%
+echo   { >> %orig_file%
+echo    "addr"  "ms.cs-viewer.ru:27010" >> %orig_file%
+echo   } >> %orig_file%
+echo   "2" >> %orig_file%
+echo   { >> %orig_file%
+echo    "addr"  "ms.cs-viewer.ru:27010" >> %orig_file%
+echo   } >> %orig_file%
+echo  } >> %orig_file%
+echo } >> %orig_file%
 goto:eof
